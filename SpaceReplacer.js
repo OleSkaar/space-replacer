edf48729ca109399ded029ff1294932323e4797c
Ole Skaar <olejohska@gmail.com>
2017-03-07 14:45:40 +0100

    Code cleanup
    Removed some commented-out code

--- SpaceReplacer.js
+++ SpaceReplacer.js
@@ -1,20 +1,16 @@
-
-
-//1.0 Window prompt method: 
 /*
+1.0 Window prompt method: 
 var prompt;
 var stringToBeReplaced = prompt('Paste a string here', 'default');
 var newString = stringToBeReplaced.replace(/ /g, '_');
 prompt('Voila:', newString);
-*/
 
-//2.0 onpaste event listener
-/*
+2.0 onpaste event listener
+
 Open page, autofocus on field
 On paste, replace spaces with underscores on text in field
 Select text in field after replacement
 */
-
 
 var inputfield = document.getElementById('inputfield');
 
@@ -24,15 +20,3 @@ function replacer() {
         inputfield.select();
     }, 4); 
 }
-
-
-
-//var stringToBeReplaced = inputfield.value;
-
-//var newString = stringToBeReplaced.replace(/ /g, '_');
-
-
-
-//inputfield.
-
-//inputfield.select();
