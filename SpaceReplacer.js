b968a4ef255d60258d412e054640dfe1edebbe4f
Ole Skaar <olejohska@gmail.com>
2017-03-07 14:39:01 +0100

    Space Replacer 2.0
    The tool no longer relies on the prompt method. 
    It now uses an "onpaste" event listener in the markup, which triggers the replace function after a 4ms delay.
    The delay is added because "onpaste" triggers directly after the paste event, before the text is pasted. 4ms is the minimum delay of the setTimeout function as per HTML 5: https://developer.mozilla.org/en-US/docs/Web/API/WindowOrWorkerGlobalScope/setTimeout#Minimum_delay_and_timeout_nesting
    The can now complete space replacement in 4 steps:
    1. Open Space Replacer
    2. Ctrl+V
    3. Ctrl+C
    4. Close Space Replacer (Ctrl+W)

--- SpaceReplacer.js
+++ SpaceReplacer.js
@@ -1,4 +1,38 @@
+
+
+//1.0 Window prompt method: 
+/*
 var prompt;
 var stringToBeReplaced = prompt('Paste a string here', 'default');
 var newString = stringToBeReplaced.replace(/ /g, '_');
 prompt('Voila:', newString);
+*/
+
+//2.0 onpaste event listener
+/*
+Open page, autofocus on field
+On paste, replace spaces with underscores on text in field
+Select text in field after replacement
+*/
+
+
+var inputfield = document.getElementById('inputfield');
+
+function replacer() {
+    setTimeout(function () {
+        inputfield.value = inputfield.value.replace(/ /g, '_');
+        inputfield.select();
+    }, 4); 
+}
+
+
+
+//var stringToBeReplaced = inputfield.value;
+
+//var newString = stringToBeReplaced.replace(/ /g, '_');
+
+
+
+//inputfield.
+
+//inputfield.select();
